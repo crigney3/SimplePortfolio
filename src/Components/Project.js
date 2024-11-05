598e0afdac370e9696e53be45ef942c7f0f65d01
Corey Rigney <coreyrigney@gmail.com>
2024-11-05 14:20:12 -0800

    Added fonts, changed project layout to flexbox

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -35,16 +35,34 @@ const Project = ({
     // Website Link
     // Itch.IO Link
 
+    const [backgroundColor, setBackgroundColor] = useState('white');
+
+    useEffect(() => {
+        calcBackgroundColor();
+    }, []);
+
+    const calcBackgroundColor = () => {
+        let numID = id.match(/^\d+|\d+\b|\d+(?=\w)/g).map(function (v) {return +v;});
+        let bgColor = numID[0] % 2 ? '#282c34' : '#1D1128';
+        setBackgroundColor(bgColor);    
+    }
+
     return (
-        <div className='Project' id={id}>
+        <div className='Project' id={id} style={{backgroundColor: backgroundColor}}>
             {(titleText !== "") && <TitleBox titleText={titleText}/>}
-            {(descriptionText !== "") && <DescriptionBox descriptionText={descriptionText}/>}
-            {(photoGalleryImageArray !== null) && <PhotoGallery GalleryImageArray={photoGalleryImageArray}/>}
-            {(videoLink !== "") && <DemoVideo videoSource={videoLink}/>}
-            {(websiteDest !== "") && (websiteText !== "") && <WebsiteLink destination={websiteDest} text={websiteText}/>}
-            {(githubDest !== "") && (githubText !== "") && <GithubLink destination={githubDest} text={githubText}/>}
-            {(downloadDest !== "") && (downloadText !== "") && <DownloadLink destination={downloadDest} text={downloadText}/>}
-            {(itchDest !== "") && (itchText !== "") && <ItchLink destination={itchDest} text={itchText}/>}
+            <div className='DescAndGallery'>
+                {(descriptionText !== "") && <DescriptionBox descriptionText={descriptionText}/>}
+                <div className='Gallery'>
+                    {(photoGalleryImageArray !== null) && <PhotoGallery GalleryImageArray={photoGalleryImageArray}/>}
+                    {(videoLink !== "") && <DemoVideo videoSource={videoLink}/>}
+                </div>                    
+            </div>
+            <div className='Links'>
+                {(websiteDest !== "") && (websiteText !== "") && <WebsiteLink destination={websiteDest} text={websiteText}/>}
+                {(githubDest !== "") && (githubText !== "") && <GithubLink destination={githubDest} text={githubText}/>}
+                {(downloadDest !== "") && (downloadText !== "") && <DownloadLink destination={downloadDest} text={downloadText}/>}
+                {(itchDest !== "") && (itchText !== "") && <ItchLink destination={itchDest} text={itchText}/>}
+            </div>
         </div>
     );
 }
